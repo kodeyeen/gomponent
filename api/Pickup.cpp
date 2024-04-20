e3b350d5b87f10312c31826589dde2c37249fd45
iscanderoff <scanderoff@gmail.com>
2024-04-21 01:00:40 +0800

    Pickup methods

--- api/Pickup.cpp
+++ api/Pickup.cpp
@@ -8,62 +8,92 @@ extern "C"
 
 // Pickup
 
-	GOMPONENT_EXPORT void* pickup_create(int modelId, unsigned char type, float x, float y, float z, unsigned int virtualWorld, int isStatic, void* player)
+	GOMPONENT_EXPORT void* pickup_create(int modelId, uint8_t type, float posX, float posY, float posZ, uint32_t virtualWorld, int isStatic)
 	{
 		auto gamemode = Gomponent::Get()->getGamemode();
-		IPickupsComponent* pickups = gamemode->pickups;
+		IPickupsComponent* component = gamemode->pickups;
 
-		if (player == NULL)
+		if (component)
 		{
-			if (pickups)
-			{
-				int id = pickups->reserveLegacyID();
-				if (id == INVALID_PICKUP_ID)
-				{
-					return NULL;
-				}
+			IPickup* pickup = component->create(modelId, type, Vector3(posX, posY, posZ), virtualWorld, false);
 
-				IPickup* pickup = pickups->create(modelId, type, Vector3(x, y, z), virtualWorld, isStatic != 0);
-				if (pickup)
-				{
-					pickups->setLegacyID(id, pickup->getID());
-					return pickup;
-				}
-				else
-				{
-					pickups->releaseLegacyID(id);
-					return NULL;
-				}
-			}
-		}
-		else
-		{
-			auto data = queryExtension<IPlayerPickupData>(static_cast<IPlayer*>(player));
-
-			if (pickups && data)
-			{
-				int id = data->reserveLegacyID();
-				if (id == INVALID_PICKUP_ID)
-				{
-					return NULL;
-				}
-
-				IPickup* pickup = pickups->create(modelId, type, Vector3(x, y, z), virtualWorld, isStatic != 0);
-				if (pickup)
-				{
-					data->setLegacyID(id, pickup->getID());
-					pickup->setLegacyPlayer(static_cast<IPlayer*>(player));
-					return pickup;
-				}
-				else
-				{
-					data->releaseLegacyID(id);
-					return NULL;
-				}
-			}
+			return static_cast<void*>(pickup);
 		}
 
 		return NULL;
+	}
+
+	GOMPONENT_EXPORT void pickup_release(void* pickup)
+	{
+		auto gamemode = Gomponent::Get()->getGamemode();
+		IPickupsComponent* component = gamemode->pickups;
+
+		if (component)
+		{
+			component->release(static_cast<IPickup*>(pickup)->getID());
+		}
+	}
+
+	GOMPONENT_EXPORT int pickup_getID(void* pickup)
+	{
+		return static_cast<IPickup*>(pickup)->getID();
+	}
+
+	GOMPONENT_EXPORT void pickup_setType(void* pickup, uint8_t type)
+	{
+		return static_cast<IPickup*>(pickup)->setType(type);
+	}
+
+	GOMPONENT_EXPORT uint8_t pickup_getType(void* pickup)
+	{
+		return static_cast<IPickup*>(pickup)->getType();
+	}
+
+	GOMPONENT_EXPORT void pickup_setModel(void* pickup, int model)
+	{
+		return static_cast<IPickup*>(pickup)->setModel(model);
+	}
+
+	GOMPONENT_EXPORT int pickup_getModel(void* pickup)
+	{
+		return static_cast<IPickup*>(pickup)->getModel();
+	}
+
+	GOMPONENT_EXPORT unsigned char pickup_isStreamedInForPlayer(void* pickup, void* player)
+	{
+		return static_cast<IPickup*>(pickup)->isStreamedInForPlayer(*static_cast<IPlayer*>(player)) ? 1 : 0;
+	}
+
+	GOMPONENT_EXPORT void pickup_setPickupHiddenForPlayer(void* pickup, void* player, unsigned char hidden)
+	{
+		return static_cast<IPickup*>(pickup)->setPickupHiddenForPlayer(*static_cast<IPlayer*>(player), hidden != 0);
+	}
+
+	GOMPONENT_EXPORT unsigned char pickup_isPickupHiddenForPlayer(void* pickup, void* player)
+	{
+		return static_cast<IPickup*>(pickup)->isPickupHiddenForPlayer(*static_cast<IPlayer*>(player)) ? 1 : 0;
+	}
+
+	// entity
+
+	GOMPONENT_EXPORT void pickup_setPosition(void* pickup, float posX, float posY, float posZ)
+	{
+		return static_cast<IPickup*>(pickup)->setPosition(Vector3(posX, posY, posZ));
+	}
+
+	GOMPONENT_EXPORT Vector3 pickup_getPosition(void* pickup)
+	{
+		return static_cast<IPickup*>(pickup)->getPosition();
+	}
+
+	GOMPONENT_EXPORT void pickup_setVirtualWorld(void* pickup, int vw)
+	{
+		return static_cast<IPickup*>(pickup)->setVirtualWorld(vw);
+	}
+
+	GOMPONENT_EXPORT int pickup_getVirtualWorld(void* pickup)
+	{
+		return static_cast<IPickup*>(pickup)->getVirtualWorld();
 	}
 
 #ifdef __cplusplus
