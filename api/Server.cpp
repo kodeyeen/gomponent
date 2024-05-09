6e2a2dd20e37724ce2e7d0b7654af408ca2e3ce8
iscanderoff <scanderoff@gmail.com>
2024-05-09 15:08:28 +0800

    server_printLnU8 and server_logLnU8 functions

--- api/Server.cpp
+++ api/Server.cpp
@@ -6,6 +6,30 @@ extern "C"
 {
 #endif
 
+	GOMPONENT_EXPORT void server_printLnU8(const char* fmt, ...)
+	{
+		ICore* core = Gomponent::Get()->core;
+
+		va_list args;
+		va_start(args, fmt);
+
+		core->vprintLnU8(fmt, args);
+
+		va_end(args);
+	}
+
+	GOMPONENT_EXPORT void server_logLnU8(int logLevel, const char* fmt, ...)
+	{
+		ICore* core = Gomponent::Get()->core;
+
+		va_list args;
+		va_start(args, fmt);
+
+		core->vlogLnU8(LogLevel(logLevel), fmt, args);
+
+		va_end(args);
+	}
+
 	GOMPONENT_EXPORT void server_setModeText(String text)
 	{
 		ICore* core = Gomponent::Get()->core;
