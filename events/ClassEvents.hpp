363e372ff187d3db4e41ccb3a47f6bc267f537da
iscanderoff <scanderoff@gmail.com>
2024-05-10 22:04:12 +0800

    Remove unnecessary check

--- events/ClassEvents.hpp
+++ events/ClassEvents.hpp
@@ -20,11 +20,6 @@ public:
 		if (classes)
 		{
 			_class = classes->get(classId);
-		
-			if (!_class)
-			{
-				_class = NULL;
-			}
 		}
 
 		return gamemode_->call<unsigned char>("onPlayerRequestClass", static_cast<void*>(&player), static_cast<void*>(_class)) != 0;
